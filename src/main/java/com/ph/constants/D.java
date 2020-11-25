09f766a248331972da80ea06fcf006f263bcbd5a
pangh <kungfu_star@sina.com>
2020-11-25 18:12:17 +0800

    测试

---
+++ src/main/java/com/ph/constants/D.java
@@ -0,0 +1,15 @@
+package com.ph.constants;
+
+/**
+ * @Description D
+ * @Author pangh
+ * @Date 2020年11月25日
+ * @Version v1.0.0
+ */
+public class D {
+
+    public static void main(String[] args) {
+        System.out.println("dev1125-01 第二次拉取出来的分支，做BUG修复...");
+    }
+
+}
