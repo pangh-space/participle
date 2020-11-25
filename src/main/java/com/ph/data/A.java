02d9f3cd54cd32b5356bfbd41f725ce1f2f4fad7
pangh <kungfu_star@sina.com>
2020-11-25 17:24:33 +0800

    第一版本BUG修复

---
+++ src/main/java/com/ph/data/A.java
@@ -0,0 +1,10 @@
+package com.ph.data;
+
+/**
+ * @Description A
+ * @Author pangh
+ * @Date 2020年11月25日
+ * @Version v1.0.0
+ */
+public class A {
+}
---
+++ src/main/java/com/ph/data/B.java
@@ -0,0 +1,10 @@
+package com.ph.data;
+
+/**
+ * @Description B
+ * @Author pangh
+ * @Date 2020年11月25日
+ * @Version v1.0.0
+ */
+public class B {
+}
