5eadd3d8b68fd4403b449eb1ea098b847f31da83
pangh <kungfu_star@sina.com>
2020-11-26 09:08:17 +0800

    测试环境测试有问题，再次做修复

--- src/main/java/com/ph/data/A.java
+++ src/main/java/com/ph/data/A.java
@@ -14,6 +14,7 @@ public class A {
     public static void main(String[] args) {
         System.out.println("修复第一版BUG");
         System.out.println("V1.0.1 线上BUG紧急修复");
+        System.out.println("V1.0.1 线上BUG测试有问题，再次做修复");
     }
 
 }
